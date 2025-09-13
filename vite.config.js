010e956f3cc9ca55a7ded9e57e5cffa4e4a5a878
Luis <blash3g@gmail.com>
2025-09-12 22:42:56 -0600

    feat: base defesen tower

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/proyectos/defenseTower/' : '/',
   plugins: [
     react(),
     tailwindcss(),
